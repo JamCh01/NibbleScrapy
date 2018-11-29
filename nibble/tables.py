bbc62c523687e949c58d280098cc05d1ab685066
JamCh01 <me@jamchoi.cc>
2018-11-29 16:32:10 +0800

    delete hash column

--- nibble/tables.py
+++ nibble/tables.py
@@ -5,9 +5,7 @@ from sqlalchemy import Column, Integer, String, Text
 class ContentTemplate():
     id = Column(Integer, primary_key=True)
     content = Column(Text)
-    content_hash = Column(String(32))
     url = Column(Text)
-    url_hash = Column(String(32))
     author = Column(String(50))
     work = Column(String(50))
     status = Column(Integer)
