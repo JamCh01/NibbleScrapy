c5b81c3e9e45c5789eacb592f1536d48f84fb90f
JamCh01 <me@jamchoi.cc>
2018-11-29 16:28:16 +0800

    delete md5 in spiders

--- nibble/spiders/duanwenxue.py
+++ nibble/spiders/duanwenxue.py
@@ -1,6 +1,4 @@
-import re
 import scrapy
-import hashlib
 from bs4 import BeautifulSoup
 from nibble.items import ContentItem
 
@@ -9,10 +7,6 @@ class Duanwenxue(scrapy.Spider):
     name = "duanwenxue"
     allowed_domains = ['www.duanwenxue.com']
     start_urls = ['https://www.duanwenxue.com/yulu/yijuhua/']
-
-    def __init__(self):
-        self.punctuation_regex = re.compile(
-            r'[\s+\.\!\/_,$%^*(+\"\')]+|[+——()?【】“”！，。？、~@#￥%……&*（）]+')
 
     def parse(self, response):
         soup = BeautifulSoup(markup=response.text, features='lxml')
@@ -34,8 +28,6 @@ class Duanwenxue(scrapy.Spider):
                 url=response.urljoin(next_url), callback=self.parse)
 
     def parse_item(self, response):
-        url = response.url
-        url_hash = hashlib.md5(url.encode('utf8')).hexdigest()
         item = ContentItem()
         soup = BeautifulSoup(markup=response.text, features='lxml')
         try:
@@ -46,14 +38,9 @@ class Duanwenxue(scrapy.Spider):
             soup.find(name='div', attrs={
                 'class': 'article-content'
             }).find(name='p', attrs=False).text.strip().split())
-        content_without_punctuation = self.punctuation_regex.sub('', content)
-        content_hash = hashlib.md5(
-            content_without_punctuation.encode('utf8')).hexdigest()
         item['content'] = content
-        item['url'] = url
+        item['url'] = response.url
         item['author'] = ''
-        item['content_hash'] = content_hash
-        item['url_hash'] = url_hash
         item['work'] = ''
         item['platform'] = 2
         item['status'] = 300
